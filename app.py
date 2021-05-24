46cd4b78bdf0546c077adc50762d663930a9e6e0
ayong <007ayongsvip@gmail.com>
2021-05-24 18:52:16 +0800

    按输入ID生成二维码

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ ISOTIMEFORMAT = '%y%m%d'
 theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
 print(theTime)
 # 链接 https://store.lizhi.io/site/products/id/31?cid=53qvofdc&mtm_campaign=wechat&mtm_kwd=p210413
-url = "https://store.lizhi.io/site/products/id/31?cid=53qvofdc&mtm_campaign=wechat&mtm_kwd=p"
-print(url+theTime)
-newurl = url + theTime
+Product_url = "https://store.lizhi.io/site/products/id/"
+id = input("请输入商品 ID：")
+url = Product_url + id + "?cid=53qvofdc&mtm_campaign=wechat&mtm_kwd=p" + theTime
+print(url)
+# 引入处理图片的模块
 from PIL import Image, ImageDraw, ImageFont
 import qrcode
-img = qrcode.make(data=newurl)
+img = qrcode.make(data=url)
 with open('test.png', 'wb') as f:
     img.save(f)
 
