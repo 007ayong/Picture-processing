ada78092f41a1655da58db5456a95c256deb87a2
ayong <007ayongsvip@gmail.com>
2021-11-09 09:36:17 +0800

    矫正二维码重复错误；更正时间错误

--- 推广联盟助手.user.js
+++ 推广联盟助手.user.js
@@ -21,31 +21,25 @@ beasetag.appendChild(mybutton);
 mybutton.innerHTML = "更改价格";
 mybutton.style = "position:fixed;bottom:17px;right:15px;width:102px;height:40px;background:#f92e37;color:white;text-align:center;line-height:40px;cursor:pointer;border-radius:8%;z-index:9999;";
 mybutton.onclick = function change_price(){
-    var new_price = prompt("请输入价格，如：6.18 起");
+    var new_price = prompt("请输入价格，如：6.18 秒杀");
     var price = document.getElementsByClassName("text-blue-600 text-22 font-semibold");
     price[0].innerHTML='<span class="text-14 font-normal">￥</span>'+new_price;
 }
 
-function Curentdate()
-    { 
-        var now = new Date();
-        var year = now.getFullYear();       //年
-        var month = now.getMonth() + 1;     //月
-        var day = now.getDate();            //日       
-        var date = year;
-       
-        if(month < 10)
-            date += "0";
-       
-        date += month;
-       
-        if(day < 10)
-            date += "0";
-           
-        date += day;
-     
-        return(date); 
-    } 
+function Curentdate(){
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    if(month < 10){
+       month = "0" + month;
+    }
+    if(day < 10){
+        day = "0" + day;
+    }
+    var date = year.toString().substr(2) + month + day;
+    return(date);
+}
 console.log("系统时间：" + Curentdate());
 
 var qrcode_button = document.createElement("div");
@@ -54,11 +48,11 @@ qrcode_button.style = "position:fixed;bottom:74px;right:15px;width:102px;height:
 beasetag.appendChild(qrcode_button);
 qrcode_button.onclick = function change_code() {
     var image = document.getElementsByClassName("pt-17 pb-12 px-12 relative")[0].children[4];
-    image.removeChild(image.children[0]);
+    image.removeChild(image.getElementsByTagName("img")[0]);
     image.style="height:90px;width:90px;background:white;padding:5px;"
-    //var url = prompt("请输入链接");
-    var pdl = document.getElementsByClassName("inline-block text-14 leading-normal font-medium py-5 px-16 text-white bg-red-500 rounded-2")[0].href;
-    var url = "https://store.lizhi.io/site/products/id/" + pdl.match(/[0-9]+/)[0] + "?cid=53qvofdc" + "&mtm_campaign=wechat&mtm_kwd=p" + Curentdate().toString().substr(2);
+    var url = prompt("请输入链接");
+    // var pdl = document.getElementsByClassName("inline-block text-14 leading-normal font-medium py-5 px-16 text-white bg-red-500 rounded-2")[0].href;
+    // var url = "https://store.lizhi.io/site/products/id/" + pdl.match(/[0-9]+/)[0] + "?cid=53qvofdc" + "&mtm_campaign=wechat&mtm_kwd=p" + Curentdate().toString().substr(2);
     // new QRCode(image, url);
     new QRCode(image, {
         text: url,
@@ -76,7 +70,7 @@ save_button.style = "position:fixed;bottom:131px;right:15px;width:102px;height:4
 beasetag.appendChild(save_button);
 save_button.onclick = function screenshot() {
     var card = document.getElementsByClassName("bg-no-repeat bg-center bg-contain px-18 py-20 mx-auto")[0];
-    var name = Curentdate().toString().substr(2) + "_" + document.getElementsByClassName("font-bold leading-none text-24")[0].innerHTML + "_qrcode";
+    var name = Curentdate()+ "_" + document.getElementsByClassName("font-bold leading-none text-24")[0].innerHTML + "_qrcode";
     var icon = document.getElementsByClassName("w-105 h-105")[0];
     icon.setAttribute("crossorigin","anonymous");
     icon.src += "?";
